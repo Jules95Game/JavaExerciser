331ac1194d067cf06b714e878493dd4f94163601
Jules95Game <romp.julian1@gmail.com>
2023-12-05 13:14:33 +0100

    Initial Files

---
+++ TestProject.ClassLibrary/Models/GreetingClass.cs
@@ -0,0 +1,8 @@
+﻿namespace TestProject.ClassLibrary.Models;
+
+public class GreetingClass
+{
+    public int Id { get; set; }
+
+    public string[] GreetingStrings { get; set; } = ["Hello", "world!", "How", "are", "you?"];
+}
---
+++ TestProject.WebAPI/Controllers/GreetingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.ClassLibrary.Models;
+
+namespace TestProject.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GreetingController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetGreeting()
+    {
+        return Ok(new GreetingClass() { Id = 1 });
+    }
+}
