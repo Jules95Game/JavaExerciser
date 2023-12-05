2d7e7e200455628efe5924c2365b580c56613b9b
Jules95Game <romp.julian1@gmail.com>
2023-12-05 15:25:37 +0100

    Add Datacontext with DbSet and seeding

--- JavaExerciser.DataAccess/DataContext.cs
+++ JavaExerciser.DataAccess/DataContext.cs
@@ -1,6 +1,22 @@
-﻿namespace JavaExerciser.DataAccess;
+﻿using JavaExerciser.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class DataContext
+namespace JavaExerciser.DataAccess;
+
+public class DataContext : DbContext
 {
+    public DbSet<Problem> Problems { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        modelBuilder.Entity<Problem>().HasData(
+            new()
+            {
+                Id = 1,
+                Name = "FindPrimeNumbers"
+            },
+            new()
+            {
+                Id = 2,
+                Name = "FormatStrings"
+            });
 }
