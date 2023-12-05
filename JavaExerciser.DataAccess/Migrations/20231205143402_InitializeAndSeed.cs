7a4ae3b5a51921346df147c254856acb41034eeb
Jules95Game <romp.julian1@gmail.com>
2023-12-05 15:35:21 +0100

    Added initial migrations

---
+++ JavaExerciser.DataAccess/Migrations/20231205143402_InitializeAndSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace JavaExerciser.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Problems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problems", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Problems",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "FindPrimeNumbers" },
+                    { 2, "FormatStrings" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Problems");
+        }
+    }
+}
