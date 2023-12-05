4656291deb4be392760bf3d622dd447e3247e16d
Jules95Game <romp.julian1@gmail.com>
2023-12-05 16:02:43 +0100

    Inject DataContext in ProblemsController

--- JavaExerciser.API/Controllers/ProblemsController.cs
+++ JavaExerciser.API/Controllers/ProblemsController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JavaExerciser.DataAccess;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace JavaExerciser.API.Controllers
+namespace JavaExerciser.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProblemsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProblemsController : ControllerBase
+    private readonly DataContext _dataContext;
+
+    public ProblemsController(DataContext dataContext)
     {
+        _dataContext = dataContext;
     }
 }
