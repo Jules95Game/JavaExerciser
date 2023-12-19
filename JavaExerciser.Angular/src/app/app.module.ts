7e311be9b7ecaa5ead91e91f0618b8e555bf929d
Jules95Game <romp.julian1@gmail.com>
2023-12-19 14:53:21 +0100

    Added ProblemList component

--- JavaExerciser.Angular/src/app/app.module.ts
+++ JavaExerciser.Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent
   ],
   imports: [
     BrowserModule,
---
+++ JavaExerciser.Angular/src/app/components/problem-list/problem-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-problem-list',
+  templateUrl: './problem-list.component.html',
+  styleUrl: './problem-list.component.scss'
+})
+export class ProblemListComponent {
+
+}
